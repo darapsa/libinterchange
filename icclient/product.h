8b8fcdd17bf243440f52bd74e447afcface8b107
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2019-09-26 11:52:37 +0800

    Fix prefixes

--- icclient/product.h
+++ icclient/product.h
@@ -1,24 +1,24 @@
 #ifndef ICCLIENT_PRODUCT_H
 #define ICCLIENT_PRODUCT_H
 
-struct ic_product {
+struct icclient_product {
 	char *sku, *description, *comment, *image;
 	double price;
 };
 
-struct ic_catalog {
+struct icclient_catalog {
 	size_t length;
-	struct ic_product *products[];
+	struct icclient_product *products[];
 };
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	void icclient_product_all(struct ic_catalog **catalogptr
+	void icclient_product_all(struct icclient_catalog **catalogptr
 			, size_t (*callback)(void *contents, size_t size
 				, size_t nmemb, void *userdata));
-	void rtclient_product_freecatalog(struct ic_catalog *catalog);
+	void icclient_product_freecatalog(struct icclient_catalog *catalog);
 
 #ifdef __cplusplus
 }
--- product.c
+++ product.c
@@ -2,16 +2,18 @@
 #include "request.h"
 #include "icclient/product.h"
 
-void icclient_product_all(struct ic_catalog **catalogptr
+typedef struct icclient_catalog icclient_catalog;
+
+void icclient_product_all(icclient_catalog **catalogptr
 		, size_t (*callback)(void *, size_t, size_t, void *))
 {
 	request(NULL, NULL, NULL, "%s", "All-Products");
 }
 
-void rtclient_product_freecatalog(struct ic_catalog *catalog)
+void icclient_product_freecatalog(icclient_catalog *catalog)
 {
 	for (size_t i = 0; i < catalog->length; i++) {
-		struct ic_product *product = catalog->products[i];
+		struct icclient_product *product = catalog->products[i];
 		if (product->image)
 			free(product->image);
 		if (product->comment)
