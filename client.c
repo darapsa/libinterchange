bf59ffd31aeb3a328a489205ecbc45c194cfa5dc
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2019-09-27 21:46:09 +0800

    Pass the data and function pointers in allproducts

--- client.c
+++ client.c
@@ -35,7 +35,7 @@ bool icclient_init(const char *url, const char *certificate)
 void icclient_allproducts(icclient_catalog **catalogptr
 		, size_t (*callback)(void *, size_t, size_t, void *))
 {
-	request(NULL, NULL, NULL, "%s", "All-Products");
+	request((void *)catalogptr, callback, NULL, "%s", "All-Products");
 }
 
 void icclient_newaccount(const char *username, const char *password
