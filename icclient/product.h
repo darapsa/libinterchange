2ed1fce21c24a54bf09da8b39efd7d889a0b74e3
ꦌ ꦫꦶ ꦏ꧀ꦦꦿ ꦧ ꦮ ꦑ ꦩ ꦭ꧀ <erik@darapsa.co.id>
2020-06-16 09:40:10 +0800

    Product cross-sell flexible array member

--- icclient/product.h
+++ icclient/product.h
@@ -1,6 +1,11 @@
 #ifndef ICCLIENT_PRODUCT_H
 #define ICCLIENT_PRODUCT_H
 
+struct icclient_product_cross_sell {
+	size_t length;
+	char *skus[];
+};
+
 struct icclient_product {
 	char *sku;
 	char *description;
@@ -10,6 +15,7 @@ struct icclient_product {
 	char *prod_group;
 	double weight;
 	char *author;
+	struct icclient_product_cross_sell *cross_sell;
 };
 
 #endif // ICCLIENT_PRODUCT_H
