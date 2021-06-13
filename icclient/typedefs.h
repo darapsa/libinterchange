88a6a79857daa0b2b2b1b03b7c8bbca8a3f6b78c
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2021-06-13 17:48:47 +0800

    C++ doesn't know size_t by default

--- icclient/typedefs.h
+++ icclient/typedefs.h
@@ -1,6 +1,9 @@
 #ifndef ICCLIENT_TYPEDEFS_H
 #define ICCLIENT_TYPEDEFS_H
 
+#ifdef __cplusplus
+#include <cstddef>
+#endif
 #ifdef __EMSCRIPTEN__
 #include <emscripten/fetch.h>
 typedef emscripten_fetch_t icclient_fetch_t;
